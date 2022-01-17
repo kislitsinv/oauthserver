d7c97025bd5507549891fcb1fe47327b9e67a535
Vladimir Kislitsin <v.kislitsin@forward.lc>
2022-01-17 15:27:05 +0600

    jwt token initial

--- src/main/java/app/candr/authorizationserver/config/AuthorizationServerConfiguration.java
+++ src/main/java/app/candr/authorizationserver/config/AuthorizationServerConfiguration.java
@@ -3,14 +3,18 @@ package app.candr.authorizationserver.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.sql.DataSource;
 
@@ -24,10 +28,21 @@ public class AuthorizationServerConfiguration implements AuthorizationServerConf
     @Autowired
     private AuthenticationManager authenticationManager;
 
-
     // IF WE WANT TO STORE TOKENS IN DB
+    //@Bean
+    //TokenStore jdbcTokenStore() {return new JdbcTokenStore(dataSource);}
+
     @Bean
-    TokenStore jdbcTokenStore() {return new JdbcTokenStore(dataSource);}
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("dummy_sign");
+        return converter;
+    }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -46,6 +61,18 @@ public class AuthorizationServerConfiguration implements AuthorizationServerConf
 
         // IF WE WANT TO STORE TOKENS IN DB
         // endpoints.tokenStore(jdbcTokenStore());
-        endpoints.authenticationManager(authenticationManager);
+        //endpoints.authenticationManager(authenticationManager);
+        endpoints.tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
+                .authenticationManager(authenticationManager);
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
     }
 }
--- src/main/java/app/candr/authorizationserver/config/WebSecurityConfiguration.java
+++ src/main/java/app/candr/authorizationserver/config/WebSecurityConfiguration.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -16,6 +19,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(HttpSecurity security) throws Exception {
+        security.formLogin().disable();
+    }
 
     @Bean
     protected AuthenticationManager getAuthenticationManager() throws Exception {
